7f49fb62bc355dba734586f7203788e458fd998d
tiny-pumpkin <lilian.pereira1620@gmail.com>
2025-02-14 16:26:54 -0300

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Todo list Flutter',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         primaryColor: const Color(0xff0AB6AB),
+        scaffoldBackgroundColor: const Color(0xff151515),
         fontFamily: 'Raleway',
         textTheme: TextTheme(
           titleMedium: TextStyle(
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff0AB6AB),
+          titleTextStyle: TextStyle(
             fontFamily: 'Raleway',
             fontWeight: FontWeight.w500,
             fontSize: 18,
@@ -48,17 +57,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
         title: Text(widget.title),
         centerTitle: true,
         toolbarHeight: 70,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.more_vert),
-            color: Colors.black,
-          ),
-        ],
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 45),
@@ -76,13 +77,50 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Text(
                 formattedDate,
-                style: TextStyle(color: Colors.white, fontSize: 14),
+                style: TextStyle(
+                  color: Color(0xff757878),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w100,
+                ),
               ),
+              // Card(
+              //   elevation: 4,
+              //   shape: RoundedRectangleBorder(
+              //     borderRadius: BorderRadius.circular(12),
+              //   ),
+              //   child: Padding(
+              //     padding: const EdgeInsets.all(16),
+              //     child: Row(
+              //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //       children: [
+              //         Text(
+              //           "Meu Card",
+              //           style: TextStyle(
+              //             fontSize: 18,
+              //             fontWeight: FontWeight.w500,
+              //           ),
+              //         ),
+              //         Icon(Icons.arrow_forward_ios),
+              //       ],
+              //     ),
+              //   ),
+              // ),
+              // ListView(
+              //   children: [
+              //     GestureDetector(
+              //       child: Card(
+              //         elevation: 4,
+              //         color: Colors.red,
+              //         shape: RoundedRectangleBorder(),
+              //       ),
+              //       onTap: () {},
+              //     ),
+              //   ],
+              // ),
             ],
           ),
         ),
       ),
-      backgroundColor: Colors.black,
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(bottom: 50),
         child: SizedBox(
@@ -91,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
           child: FittedBox(
             child: FloatingActionButton(
               onPressed: () {},
-              tooltip: 'Increment',
+              tooltip: 'Create task',
               backgroundColor: Theme.of(context).primaryColor,
               shape: CircleBorder(),
               child: const Icon(Icons.add, size: 30),
